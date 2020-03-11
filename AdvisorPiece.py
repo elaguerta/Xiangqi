fe7f24e0b97857c534d378cd9f921a947e08e709
Elaine Laguerta <elaine.laguerta@gmail.com>
2020-03-11 13:16:57 -0700

    Comment Advisor

--- AdvisorPiece.py
+++ AdvisorPiece.py
@@ -1,37 +1,38 @@
 from Piece import Piece
 
 class AdvisorPiece(Piece):
-    """ Instantiates an Advisor Piece"""
-
-    # advisor_positions is a class variable, a dictionary of initial positions keyed by player color
+    """ Instantiates an Advisor Piece
+    Advisor_positions is a class variable, a dictionary of initial positions keyed by player color
+    Two Advisor pieces are created by a call to Player.__init__(), one for each of the two positions
+    per side."""
     advisor_positions = {
         'red': ['d1', 'f1'],
         'black': ['d10', 'f10']
     }
 
-    def __init__(self, player, board, id_num):
-        """ Initializes an advisor piece. The player who owns the piece, the board, and the id_num of this piece are
+    def __init__(self, side, board, id_num):
+        """ Initializes an advisor piece. The side ('red' or 'black'), the Board, and the id_num of this piece are
         passed as arguments. """
-        super().__init__(player, board) # call the Piece init with player and board
-        self._movement = 'diagonal'  # advisors can move diagonally
-        self._path_length = 1       # advisors can only move one location
+        super().__init__(side, board) # call the Piece init with side and board
+        self._movement = 'diagonal'  # Advisors can move diagonally
+        self._path_length = 1       # Advisors can only move one point
 
-        # assign a position.
-        self._id = id_num      # passed as argument, 1 or 2
-        self._pos = AdvisorPiece.advisor_positions[player][id_num - 1] # keep track of positions used
-        # and available in the Advisor class based on id number
+        # Assign a position from Advisor Piece.
+        self._id = id_num      # id_num is passed as argument, by Player, and will be 1 or 2
+        self._pos = AdvisorPiece.advisor_positions[player][id_num - 1] # keep track of positions used based on id number
 
     def __repr__(self):
-        """return an informative string about this piece ["r" or "b"] + "Ad" + [id_num for this specific piece], unique
-         for every piece in a Game """
+        """Return an informative label for this Piece: ["r" or "b"] + "Ad" + [id_num for this specific piece].
+        This is intended to be unique for every piece in a Game """
         return self._side[0] + "Ad" + str(self._id)
 
     def is_legal(self, to_pos):
-        """ check Piece legal conditions for moving to to_pos, with the additional restriction that the advisor
-        remain in the castle"""
+        """Returns True if it is legal for Advisor to move to to_pos. Checks Piece.is_legal() conditions,
+        with additional restriction that Advisors remain in the castle."""
 
         # to_pos must be a castle spot for this piece's side
         if to_pos not in (self._board.get_castle_spots(self._side)):
             return False
+
         # if it is a castle spot, check that all other legal conditions for Piece hold
         return super().is_legal(to_pos)
--- XiangqiGame.py
+++ XiangqiGame.py
@@ -1,4 +1,41 @@
 
+class AdvisorPiece(Piece):
+    """ Instantiates an Advisor Piece
+    Advisor_positions is a class variable, a dictionary of initial positions keyed by player color
+    Two Advisor pieces are created by a call to Player.__init__(), one for each of the two positions
+    per side."""
+    advisor_positions = {
+        'red': ['d1', 'f1'],
+        'black': ['d10', 'f10']
+    }
+
+    def __init__(self, side, board, id_num):
+        """ Initializes an advisor piece. The side ('red' or 'black'), the Board, and the id_num of this piece are
+        passed as arguments. """
+        super().__init__(side, board) # call the Piece init with side and board
+        self._movement = 'diagonal'  # Advisors can move diagonally
+        self._path_length = 1       # Advisors can only move one point
+
+        # Assign a position from Advisor Piece.
+        self._id = id_num      # id_num is passed as argument, by Player, and will be 1 or 2
+        self._pos = AdvisorPiece.advisor_positions[player][id_num - 1] # keep track of positions used based on id number
+
+    def __repr__(self):
+        """Return an informative label for this Piece: ["r" or "b"] + "Ad" + [id_num for this specific piece].
+        This is intended to be unique for every piece in a Game """
+        return self._side[0] + "Ad" + str(self._id)
+
+    def is_legal(self, to_pos):
+        """Returns True if it is legal for Advisor to move to to_pos. Checks Piece.is_legal() conditions,
+        with additional restriction that Advisors remain in the castle."""
+
+        # to_pos must be a castle spot for this piece's side
+        if to_pos not in (self._board.get_castle_spots(self._side)):
+            return False
+
+        # if it is a castle spot, check that all other legal conditions for Piece hold
+        return super().is_legal(to_pos)
+
 class Board:
     """ Creates a Board object for use by XiangqiGame, Players, and Pieces
     Language notes:
